81f9fa9f4a1eb64bdc6553e102757e124253849a
Jihun Suk <jhsuk0929@gmail.com>
2021-11-04 18:46:11 +0900

    HTML
    HTML for main, join, login page
    Controller fixed for the mapping

--- src/main/java/com/example/financeapp/HomeController.java
+++ src/main/java/com/example/financeapp/HomeController.java
@@ -3,6 +3,7 @@ package com.example.financeapp;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
@@ -11,8 +12,19 @@ import org.springframework.ui.Model;
 public class HomeController {
 
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/home")
     public String home(Locale locale, Model model) {
+
         return "home";
+    }
+
+    @GetMapping("/join")
+    public String join (){
+        return "join_form";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login_form";
     }
 }
