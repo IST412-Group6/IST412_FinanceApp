525cc47149218186a1be0def318904608d61d212
amcinvaille <alexa.mcinvaille@gmail.com>
2021-11-09 18:00:24 -0600

    Renamed CreateAccountController to JoinController

--- src/main/java/com/example/financeapp/controller/JoinController.java
+++ src/main/java/com/example/financeapp/controller/JoinController.java
@@ -7,13 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-public class CreateAcctController {
-    private static final Logger log = LoggerFactory.getLogger(CreateAcctController.class);
+public class JoinController {
+    private static final Logger log = LoggerFactory.getLogger(JoinController.class);
 
     @Autowired
     private UserRepository userRepository;
